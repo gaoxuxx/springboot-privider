734ecdc938129c8a5ce6bf4bde6134c44a3421a4
Your Name <email@example.com>
2018-10-30 20:41:59 +0800

    dubbo服务端

---
+++ src/main/java/com/jk/service/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jk.service;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void getUserInfo() {
+        System.out.println("username=admin");
+    }
+}
